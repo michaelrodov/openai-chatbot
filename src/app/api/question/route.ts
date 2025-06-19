0a5ccbae8ca7d31e780444caca193b231050c702
michael.rodov <rodov.michael@gmail.com>
2025-06-19 10:38:09 +0300

    - add launch configuration for VSCode and update package scripts; - added an option to bypass aifw in request - added run config for prod

--- src/app/api/question/route.ts
+++ src/app/api/question/route.ts
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 import {isEmpty} from "lodash";
-import {AIFW_EVENTS_SYSTEM_PROMPT, aiFwClient, askAiFw, askOpenAi, MODEL_NAME} from "../../utils/llmUtils";
+import {AIFW_EVENTS_SYSTEM_PROMPT, aiFwClient, askAiFw, askOpenAi, MODEL_NAME, openAiClient} from "../../utils/llmUtils";
 
 // in case the baseUrl is not provided the regular openAi url will be used (in the .env files)
 // const openAiClient = new OpenAI({
@@ -13,19 +13,30 @@ const ERROR_MESSAGE = 'Could not get a response from the AI';
 
 export const POST = async (req: Request) => {
     const propmpt = await req.text();
-    const isProtectedByAiFw = req?.headers?.get("X-is-protected") ?? false;
-    console.log(`loading openAi through: (base:${aiFwClient.baseURL}`);
 
     if (isEmpty(propmpt)) {
         return new Response('request must contain json object in the body of this type {question: "xxxxxx"}', {status: 400});
     }
 
+
+    const isProtectedByAiFw = req?.headers?.get("X-is-protected") ?? false;
+    console.log(`loading openAi through: (base:${isProtectedByAiFw ? aiFwClient.baseURL : openAiClient.baseURL})`);
+
+
     try {
         const asyncResponse = isProtectedByAiFw ? askAiFw(propmpt) : askOpenAi(propmpt);
         const aiResponse = await asyncResponse;
-        console.log('aiResponse', aiResponse);
+        console.log('aiResponse: ', JSON.stringify(aiResponse, null, 2));
         const responseText = aiResponse.choices.length > 0 ? aiResponse.choices[0]?.message?.content ?? ERROR_MESSAGE : ERROR_MESSAGE ;
-        return new Response(responseText, {status: responseText ? 200 : 500});
+        return new Response(responseText, {
+            headers: {
+                'Content-Type': 'text/plain',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Api-Key, X-is-protected'
+            },
+            status: responseText ? 200 : 500
+        });
     } catch (error) {
         console.error("Error processing request:", error);
         // @ts-ignore
